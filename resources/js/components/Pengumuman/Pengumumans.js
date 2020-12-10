1a664582b71c42ce621193add1cff2c8bd90eb93
poseisharp <firdausfajar19@gmail.com>
2020-12-10 09:46:11 +0700

    Added Pengumuman List

--- resources/js/components/Pengumuman/Pengumumans.js
+++ resources/js/components/Pengumuman/Pengumumans.js
@@ -5,7 +5,6 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Pengumuman from "../views/Pengumuman";
 
 const useStyles = makeStyles(theme => ({
     root: {
