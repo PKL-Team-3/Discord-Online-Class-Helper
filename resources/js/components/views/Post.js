2036d120c3493db3e419d3d2bdce2959e64b6e11
poseisharp <firdausfajar19@gmail.com>
2020-12-10 12:16:02 +0700

    Front End Get Done

--- resources/js/components/views/Post.js
+++ resources/js/components/views/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../layouts/Navbar";
+import Posts from "../Posts/Posts";
 
 export default function Post() {
     const role = localStorage.getItem("role");
@@ -12,8 +13,8 @@ export default function Post() {
                     Unauthorized
                 </h1>
             ) : (
-                <div>
-                    <h2>Post</h2>
+                <div className="container">
+                    <Posts />
                 </div>
             )}
         </React.Fragment>
